d05d686bdc67ddca6ed8557d49877b2a69fcb4d2
supalapka <yurabalint0103@gmail.com>
2022-01-20 21:58:44 +0200

    Split HotelsController to rooms and hotels controllers

--- BookARoom_test1/Controllers/HotelsController.cs
+++ BookARoom_test1/Controllers/HotelsController.cs
@@ -1,21 +1,15 @@
-﻿using BookARoom_test1.Areas.Identity.Data;
-using BookARoom_test1.Models;
+﻿using BookARoom_test1.Models;
 using DataLibrary;
-using DataLibrary.BusinessLogic;
 using DataLibrary.BusinessLogic.EntityFramework;
-using DataLibrary.DataAccess;
 using DataLibrary.Interface;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace BookARoom_test1.Controllers
 {
     public class HotelsController : Controller
     {
-        //HotelRepository -> Entiry
+        //HotelRepository -> Entity
         //Hotelprocessor -> Dapper
         IHotel hotelRepository = new HotelRepository();
         //IHotel hotelRepository = new Hotelprocessor();
@@ -25,29 +19,16 @@ namespace BookARoom_test1.Controllers
             hotelRepository = _hotelRepository;
         }
 
-        public IActionResult Index()
-        {
-            return RedirectToAction("List", "Hotels");
-        }
+        public IActionResult Index() { return RedirectToAction("List", "Hotels"); }
 
-        public IActionResult CreateHotel()
-        {
+        public IActionResult CreateHotel() { return View(); }
 
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult CreateHotel(HotelModel hotel)
-        {
-            hotelRepository.Create(hotel.Name, hotel.Location, hotel.Rating, hotel.RoomCount, MyFunctionsClass.FileToByteArrayAsync(hotel.PreviwImageFile));
-            return View();
-        }
 
         [Route("Hotels/")]
         public IActionResult List()
         {
             ViewBag.Message = "List";
-            var data = hotelRepository.Load(); 
+            var data = hotelRepository.Load();
             List<HotelModel> hotels = new List<HotelModel>();
 
 
@@ -64,53 +45,12 @@ namespace BookARoom_test1.Controllers
             return View(hotels);
         }
 
-        [Authorize]
-        [Route("Hotels/{id:int}")]
-        public IActionResult RoomsList(int id)
-        {
-            ViewBag.Message = "RoomsList";
-            var data = RoomProcessor.GetFreeRooms();  //load all rooms from all hotels, demo version for testing
-            List<RoomModel> rooms = new List<RoomModel>();
-
-            data.ForEach(val => rooms.Add(new RoomModel
-            {
-                Id = val.Id,
-                HotelId = val.HotelId,
-                NumberOfRooms = val.NumberOfRooms,
-                Description = val.Description,
-                Price = val.Price,
-                RoomNUmber = val.RoomNUmber
-            }));
-
-            return View(rooms);
-        }
-
-        public IActionResult Room(int id)
-        {
-            ViewData["ID"] = id;
-            var data = RoomProcessor.GetRoom(id); //get room from DataLibrary model
-
-            RoomModel room = new RoomModel //convert DataLibrary.RoomModel to this.RoomModel
-            {
-                Description = data.Description,
-                Id = data.Id,
-                NumberOfRooms = data.NumberOfRooms,
-                HotelId = data.HotelId,
-                Price = data.Price,
-                RoomNUmber = data.RoomNUmber
-            };
-
-            return View(room);
-        }
-
 
         [HttpPost]
-        public async Task<IActionResult> MakeOrder(int roomNumber)
+        public IActionResult CreateHotel(HotelModel hotel)
         {
-            SqlDataAccess.ExecuteSqlRequest($"update dbo.Rooms set IsBooked = 'true' where RoomNumber = {roomNumber}"); //mark the room as already booked
-            var user = SqlDataAccess.GetOjbect<AuthUser>("AspNetUsers", "Id", User.Identity.GetUserId());
-            BookedRoomsProcessor.Create(roomNumber, user.Email);
-            return RedirectToAction("List", "Hotels");
+            hotelRepository.Create(hotel.Name, hotel.Location, hotel.Rating, hotel.RoomCount, MyFunctionsClass.FileToByteArrayAsync(hotel.PreviwImageFile));
+            return View();
         }
 
     }
---
+++ BookARoom_test1/Controllers/RoomsController.cs
@@ -0,0 +1,70 @@
+﻿using BookARoom_test1.Areas.Identity.Data;
+using BookARoom_test1.Models;
+using DataLibrary.BusinessLogic;
+using DataLibrary.DataAccess;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookARoom_test1.Controllers
+{
+    public class RoomsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Authorize]
+        [Route("Hotels/{id:int}")]
+        public IActionResult RoomsList(int id)
+        {
+            ViewBag.Message = "RoomsList";
+            var data = RoomProcessor.GetFreeRooms();  //load all rooms from all hotels, demo version for testing
+            List<RoomModel> rooms = new List<RoomModel>();
+
+            data.ForEach(val => rooms.Add(new RoomModel
+            {
+                Id = val.Id,
+                HotelId = val.HotelId,
+                NumberOfRooms = val.NumberOfRooms,
+                Description = val.Description,
+                Price = val.Price,
+                RoomNUmber = val.RoomNUmber
+            }));
+
+            return View(rooms);
+        }
+
+        public IActionResult Room(int id)
+        {
+            ViewData["ID"] = id;
+            var data = RoomProcessor.GetRoom(id); //get room from DataLibrary model
+
+            RoomModel room = new RoomModel //convert DataLibrary.RoomModel to this.RoomModel
+            {
+                Description = data.Description,
+                Id = data.Id,
+                NumberOfRooms = data.NumberOfRooms,
+                HotelId = data.HotelId,
+                Price = data.Price,
+                RoomNUmber = data.RoomNUmber
+            };
+
+            return View(room);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> MakeOrder(int roomNumber)
+        {
+            SqlDataAccess.ExecuteSqlRequest($"update dbo.Rooms set IsBooked = 'true' where RoomNumber = {roomNumber}"); //mark the room as already booked
+            var user = SqlDataAccess.GetOjbect<AuthUser>("AspNetUsers", "Id", User.Identity.GetUserId()); //fix
+            BookedRoomsProcessor.Create(roomNumber, user.Email);
+            return RedirectToAction("List", "Hotels");
+        }
+    }
+}
